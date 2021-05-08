ffb232d6e4f0d9662bc6455cb9ac12711d6b5ece
miftahuljannah4 <81582495+miftahuljannah4@users.noreply.github.com>
2021-05-08 16:34:25 +0700

    Add files via upload

---
+++ php_dasar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+    <head> 
+        <meta charset="UTF-8"> 
+        <title>PHP Dasar</title> 
+    </head> 
+    <body> 
+        <h1>Belajar PHP Dasar</h1> 
+        <?php 
+            echo "Hello World"; ?>
+            
+        <h2> Menggunakan Variabel</h2>
+        <?php 
+            $nim = "311910740"; 
+            $nama = 'Miftahul Jannah'; 
+            echo "NIM : " . $nim . "<br>"; 
+            echo "Nama : $nama"; ?> 
+    </body> 
+</html>
